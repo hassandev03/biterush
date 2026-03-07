f255316cc4c01d422f21788831971d52edd70edb
Muhammad Hassan Raza <147485527+hassandev03@users.noreply.github.com>
2026-03-08 01:28:48 +0500

    Delete scripts directory

--- scripts/test-hash.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-async function test() {
-  const hash = '$2b$10$QvKeqovqZMpNucYCZ65tNeiHxPxQe6dwCxD31RmuksPaDOQvCgGEO';
-  const match = await bcrypt.compare('admin@br', hash);
-  console.log('Match:', match);
-}
-
-test();
